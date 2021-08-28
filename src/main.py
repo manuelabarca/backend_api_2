ee1366c7792b08bb99041a79ed02f79e8aae5ad3
Manuel Labarca <labarca.manu@gmail.com>
2021-08-28 10:02:19 +0000

    Update with examples

--- src/main.py
+++ src/main.py
@@ -8,7 +8,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet
+from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity, create_access_token
 #from models import Person
 
 app = Flask(__name__)
@@ -19,6 +20,10 @@ MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = "4geeks"  # Change this!
+jwt = JWTManager(app)
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -38,6 +43,40 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route('/planet', methods=['GET'])
+@jwt_required()
+def getPlanets():
+    planets = Planet.getAll()
+    return jsonify(planets), 200
+
+@app.route('/planet/<int:id>', methods=['GET'])
+@jwt_required()
+def getPlanet(id):
+    planet = Planet.getPlanet(id)
+ 
+@app.route('/planet/<int:id>', methods=['DELETE'])
+@jwt_required()
+def deletePlanet(id):
+    planet = Planet.deletePlanet(id)
+    return jsonify(planet), 200
+    
+
+@app.route('/login', methods=['POST'])
+def sign_in():
+    body = request.get_json()
+    if body is None:
+        return jsonify({"msg": "Body is empty"})
+    
+    email = body["email"]
+    password = body["password"]
+
+    user = User.getUser(email, password)
+    if user is None:
+        return jsonify({"msg": "Email or password incorrect"})
+    
+    token = create_access_token(identity=user.id)
+    return jsonify({"token": token}), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -9,7 +9,7 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
@@ -17,3 +17,37 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+    def getUser(email, password):
+        user = User.query.filter_by(email=email, password=password).first()
+        return user
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    size = db.Column(db.Integer, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "size": self.size
+        }
+
+    def getAll():
+        planets = Planet.query.all()
+        planets = list(map(lambda planet: planet.serialize(), planets))
+        return planets
+    
+    def getPlanet(id):
+        planet = Planet.query.get(id)
+        if planet is None:
+            return {"msg": "This planet doesn't exist"}
+        planet = Planet.serialize(planet)
+        return planet
+    
+    def deletePlanet(id):
+        planet = Planet.query.get(id)
+        db.session.delete(planet)
+        db.session.commit()
+        return {"msg": "Planet was delete"}
